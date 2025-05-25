49ed12563876f856e57f21aaa4029bd476e3d9db
Altay Hodoglugil <altay.hodo@gmail.com>
2025-05-24 18:36:33 -0700

    add search filtering

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 const express = require('express');
 const app = express();
+const db = require('./db/queries');
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 
-const addUserRouter = require('./routes/addUserRouter')
+const addUserRouter = require('./routes/addUserRouter');
 
 app.use('/new', addUserRouter);
 
-app.get('/', (req, res) => {
-  console.log('usernames will be logged here - wip')
-  res.render('index', {title: 'Home page'})
-})
+app.get('/', async (req, res) => {
+  const searchTerm = req.query.search;
+  let usernames;
+
+  if (searchTerm) {
+    usernames = await db.searchUsernames(searchTerm)
+  } else {
+    usernames = await db.getAllUsernames();
+  }
+
+  console.log('Usernames: ', usernames)
+  res.render('index', { title: 'Home page', usernames: usernames });
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Express app listening on port ${PORT}!`));
