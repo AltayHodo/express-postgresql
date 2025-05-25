f4bf063386784a97b4b8776e3c594d81e78ce8c6
Altay Hodoglugil <altay.hodo@gmail.com>
2025-05-24 18:10:55 -0700

    setup postgresql database

--- controllers/addUserController.js
+++ controllers/addUserController.js
@@ -1,8 +1,23 @@
+const db = require('../db/queries')
 
-exports.addUserGet = (req, res) => {
+async function getUsernames(req, res) {
+  const usernames = await db.getAllUsernames();
+  console.log("Usernames: ", usernames);
+  res.send("Usernames: " + usernames.map(user => user.username).join(", "));
+}
+
+async function addUserGet(req, res) {
   res.render('addUser', {title: 'Add User Form'})
 }
 
-exports.addUserPost = (req, res) => {
-  console.log("username to be saved: ", req.body.username)
+async function addUserPost(req, res) {
+  const { username } = req.body;
+  await db.insertUsername(username);
+  res.redirect("/");
 }
+
+module.exports = {
+  getUsernames,
+  addUserGet,
+  addUserPost
+};
